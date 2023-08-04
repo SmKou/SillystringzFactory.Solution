32fc17180888b2f356ec33d4998a6b52fd49ccf1
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-04 16:03:38 -0700

    WIP add and remove joins from engineers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -27,8 +27,41 @@ public class EngineersController : Controller
         return RedirectToAction("Index", "Home");
     }
 
-    // public ActionResult Details(int id) {}
-    // public ActionResult AddMachine(Engineer engineer, int machineId) {}
+    public ActionResult Details(int id) 
+    {
+        EngineerMachine join = new EngineerMachine();
+        join.EngineerId = id;
+        join.Engineer = _db.Engineers
+            .Include(engr => engr.Machines)
+            .ThenInclude(join => join.Machine)
+            .FirstOrDefault(engr => engr.EngineerId == id);
+        ViewBag.Machines = new SelectList(_db.Machines, "MachineId", "MachineName");
+        return View(engineer);
+    }
+
+    public ActionResult AddMachine(EngineerMachine join)
+    {
+        bool hasRelation = _db.EngineerMachines
+            .Any(join => join.EngineerId == join.EngineerId
+            && join.MachineId == join.MachineId);
+        if (!hasRelation)
+        {
+            _db.EngineerMachines.Add(join);
+            _db.SaveChanges();
+        }
+        return RedirectToAction("Details", new { id = engineerMachine.EngineerId });
+    }
+
+    public ActionResult RemoveMachine(int engineerId, int machineId)
+    {
+        EngineerMachine join = _db.EngineerMachines
+            .FirstOrDefault(join => join.EngineerId == engineerId
+            && join.MachineId == machineId);
+        _db.EngineerMachines.Remove(join);
+        _db.SaveChanges();
+        return RedirectToAction("Details", new { id = engineerId });
+    }
+
     // public ActionResult Edit(int id) {}
     // [HttpPost] public ActionResult Edit(Engineer engineer) {}
 
